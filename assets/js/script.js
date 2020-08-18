32025210de70eac9c27c22dcfe706dbbbd5790bb
Patrick Short <pshort0905@gmail.com>
2020-08-18 16:21:51 -0700

    createEventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,25 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 
-buttonEl.addEventListener("click", createTaskHandler);
+//buttonEl.addEventListener("click", createTaskHandler);
+
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+
 var createTaskHandler = function() {
+
+  event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+    
   }
+ 
 
-buttonEl.addEventListener("click", function() {
+formEl.addEventListener("submit",createTaskHandler); {
   var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  //listItemEl.className = "task-item";
+  //listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-});
+};
